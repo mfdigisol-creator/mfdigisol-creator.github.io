f580137aab4b979fcf83c7e4b0d78ded153b34c0
mfdigisol-creator <mfdigisol@gmail.com>
2026-08-09 16:41:11 +0500

    Fix staging Turnstile site key

--- orders-config.js
+++ orders-config.js
@@ -6,7 +6,7 @@
   window.AL_HUMA_ORDERS_CONFIG = Object.freeze(Object.assign({
     mode: 'staging',
     endpoint: 'https://alhuma-orders-staging.alhumacollection.workers.dev/v1/orders',
-    turnstileSiteKey: '0x4AAAAAAAEB8ZJE2SwNUQF6I',
+    turnstileSiteKey: '0x4AAAAAAEB8ZJE2SwNUQF6I',
     turnstileAction: 'submit_order',
     consentVersion: 1,
     confirmedUrl: 'https://alhumacollection.com/order-confirmed.html',
